90d1e8bcc5cf95a019fa144d4534f6766d877370
AndroidHQ254 <d.murimi14@gmail.com>
2023-12-14 17:21:44 +0300

    X6739: We have dynamic partitions

--- device.mk
+++ device.mk
@@ -34,6 +34,9 @@ PRODUCT_PACKAGES += \
     update_verifier \
     update_engine_sideload
 
+# Dynamic partitions
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
+
 # fastbootd
 PRODUCT_PACKAGES += \
     android.hardware.fastboot@1.0-impl-mock \
